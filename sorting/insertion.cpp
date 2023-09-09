ed782f5c033868e3f5aad1d9b4af102d55bae1d7
Glitchez <87347098+Glitchez-1984@users.noreply.github.com>
2023-09-09 16:05:02 -0400

    Update insertion.cpp

--- sorting/insertion.cpp
+++ sorting/insertion.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <vector>
 
+void insertionSort(std::vector<int>& arr) {
+    int n = arr.size();
+    
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    std::vector<int> arr = {12, 11, 13, 5, 6};
+    
+    std::cout << "Original array: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    insertionSort(arr);
+
+    std::cout << "Sorted array: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
